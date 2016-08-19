b7180af0ff628a5999721a0f48129b20af93c498
Josh Adams <josh.rubyist@gmail.com>
2016-08-19 09:32:49 -0500

    Upgrade to elm-mdl 7.5.0

--- elm/src/View/Users.elm
+++ elm/src/View/Users.elm
@@ -9,15 +9,13 @@ import Material.List as List
 
 view : Model -> Html Msg
 view model =
-    List.ul
-        []
+    List.ul []
         (List.map (viewUserRow model) model.users)
 
 
 viewUserRow : Model -> User -> Html Msg
 viewUserRow model user =
-    List.li
-        []
+    List.li []
         [ List.content []
             [ text user.name ]
         ]
