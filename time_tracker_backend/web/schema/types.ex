d6069247d6cb3b79d5b443b56a519b0cfbe37aad
Josh Adams <josh.rubyist@gmail.com>
2016-12-03 00:33:51 -0600

    Document our types a bit

--- time_tracker_backend/web/schema/types.ex
+++ time_tracker_backend/web/schema/types.ex
@@ -1,9 +1,10 @@
 defmodule TimeTrackerBackend.Schema.Types do
   use Absinthe.Schema.Notation
 
+  @desc "A user of our system"
   object :user do
     field :id, :id
-    field :name, :string
+    field :name, :string, description: "The user's earth name"
     field :gender, :string
     field :email, :string
     field :username, :string
