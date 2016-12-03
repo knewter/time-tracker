d773440667a4020f0ad3926ef32d3c7306d689ea
Josh Adams <josh.rubyist@gmail.com>
2016-12-03 00:48:09 -0600

    Add ability to find a single user

--- time_tracker_backend/web/resolvers/user_resolver.ex
+++ time_tracker_backend/web/resolvers/user_resolver.ex
@@ -4,4 +4,11 @@ defmodule TimeTrackerBackend.UserResolver do
   def all(_args, _info) do
     {:ok, Repo.all(User)}
   end
+
+  def find(%{id: id}, _info) do
+    case Repo.get(User, id) do
+      nil -> {:error, "No user found with id #{id}"}
+      user -> {:ok, user}
+    end
+  end
 end
--- time_tracker_backend/web/schema.ex
+++ time_tracker_backend/web/schema.ex
@@ -3,8 +3,15 @@ defmodule TimeTrackerBackend.Schema do
   import_types TimeTrackerBackend.Schema.Types
 
   query do
+    @desc "Get a list of users"
     field :users, list_of(:user) do
       resolve &TimeTrackerBackend.UserResolver.all/2
+    end
+
+    @desc "Get a single user"
+    field :user, type: :user do
+      arg :id, non_null(:id)
+      resolve &TimeTrackerBackend.UserResolver.find/2
     end
   end
 end
